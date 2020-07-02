4a163cb884297edd272f4f3429059247e09e94d7
Paulina <paulina12061993@gmail.com>
2020-07-02 18:54:12 +0200

    test_v2

--- footballscore_app/tests/conftest.py
+++ footballscore_app/tests/conftest.py
@@ -1,10 +1,9 @@
 import pytest
 from django.contrib.contenttypes.models import ContentType
 from django.test import Client, override_settings
-from footballscore_app.models import League, Team, Match, Country
+from footballscore_app.models import League, Team, Match, Country, Season, TeamSeason
 from django.contrib.auth.models import User, Permission
 from django.core.files import File
-
 
 
 @pytest.fixture
@@ -12,37 +11,69 @@ def client():
     client = Client()
     return client
 
+
 @pytest.fixture
 def user():
     user = User(username='paulina')
     user.set_password('ad')
     user.save()
-    p = Permission.objects.get(codename='view_league')
-    user.user_permissions.add(p)
-    p = Permission.objects.get(codename='view_team')
-    user.user_permissions.add(p)
-
-    # contenttype = ContentType.objects.get(model='genre')
-    # permissions = Permission.objects.filter(content_type=contenttype)
-    # user.user_permissions.set(permissions)
-
+    contenttype = ContentType.objects.get(model='league')
+    permissions1 = Permission.objects.filter(content_type=contenttype)
+    contenttype = ContentType.objects.get_for_model(Season)
+    permissions2 = Permission.objects.filter(content_type=contenttype)
+    contenttype = ContentType.objects.get(model='team')
+    permissions3 = Permission.objects.filter(content_type=contenttype)
+    contenttype = ContentType.objects.get(model='match')
+    permissions4 = Permission.objects.filter(content_type=contenttype)
+    user.user_permissions.set(permissions1|permissions2|permissions3|permissions4)
     return user
+
 
 @pytest.fixture
 def league():
-    s= Country.objects.create(name='Spain', flag=File(file=b""))
-    e= Country.objects.create(name='England', flag=File(file=b""))
-    ge= Country.objects.create(name='Germany', flag=File(file=b""))
-    g = [League.objects.create(name='La Liga', description='Liga hiszpańska', hierarchy='1', country=s)]
-    g.append(League.objects.create(name='Premier League', description='Liga angielska', hierarchy='1', country=e))
-    g.append(League.objects.create(name='Bundesliga', description='Liga niemiecka', hierarchy='1', country=ge))
+    s = Country.objects.create(name='Spain', flag='/uploads/flags/Spain.gif')
+    e = Country.objects.create(name='England', flag='/uploads/flags/England.png')
+    ge = Country.objects.create(name='Germany', flag='/uploads/flags/Germany.png')
+    g = [League.objects.create(pk = 1, name='La Liga', description='Liga hiszpańska', hierarchy='1', country=s)]
+    g.append(League.objects.create(pk = 2, name='Premier League', description='Liga angielska', hierarchy='1', country=e))
+    g.append(League.objects.create(pk = 3, name='Bundesliga', description='Liga niemiecka', hierarchy='1', country=ge))
     return g
 
 
 @pytest.fixture
 def team():
-    a=[Team.objects.create(name='Bayern', description='Niemiecka liga', coach='JJ', arms='/uploads/uploads/FC_Barcelona.png',
-                                 address='Bayern, Germany')]
-    a.append(Team.objects.create(name='Liverpool', description='Angielska Liga', arms='/uploads/uploads/FC_Barcelona.png', coach='JJ', address='Liverpool, England'))
+    a = [Team.objects.create(pk=1, name='Bayern', description='Niemiecka liga', coach='JJ',
+                             arms='/uploads/uploads/FC_Barcelona.png',
+                             address='Bayern, Germany')]
+    a.append(
+        Team.objects.create(pk=2, name='Liverpool', description='Angielska Liga', arms='/uploads/uploads/FC_Barcelona.png',
+                            coach='JJ', address='Liverpool, England'))
     return a
 
+@pytest.fixture
+def season():
+    s = Country.objects.create(name='Spain', flag=File(file=b""))
+    league = League.objects.create(name='La Liga', description='Liga hiszpańska', hierarchy='1', country=s)
+    season = Season.objects.create(
+        pk = 1,
+        season="2019/2020",
+        league=league, )
+    return season
+
+@pytest.fixture
+def match():
+    country = Country.objects.create(name='Spain', flag=File(file=b""))
+    league = League.objects.create(name='La Liga', description='Liga hiszpańska', hierarchy='1', country=country)
+    season = Season.objects.create(
+        season="2019/2020",
+        league=league)
+    team1 = Team.objects.create(name='Bayern', description='Niemiecka liga', coach='JJ',
+                        arms='/uploads/uploads/FC_Barcelona.png',
+                        address='Bayern, Germany')
+    team2 = Team.objects.create(name='Bayern', description='Niemiecka liga', coach='JJ',
+                                arms='/uploads/uploads/FC_Barcelona.png',
+                                address='Bayern, Germany')
+    th = TeamSeason.objects.create(team=team1, season=season)
+    ta = TeamSeason.objects.create(team=team2, season=season)
+    match = Match.objects.create(pk=1, team_home=th, team_away=ta, date='2020-07-01')
+    return match
--- footballscore_app/tests/test_app.py
+++ footballscore_app/tests/test_app.py
@@ -5,20 +5,138 @@ def test_index(client):
     response = client.get('/footballscore/')
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
 def test_league(client, user, league):
-    client.login(username = 'paulina', password = 'ad')
+    client.login(username='paulina', password='ad')
     response = client.get('/footballscore/league/')
     assert response.status_code == 200
     assert len(response.context['objects']) == 3
     for i in range(len(league)):
         assert league[i] in response.context['objects']
 
+
 @pytest.mark.django_db
 def test_team(client, user, team):
-    client.login(username = 'paulina', password = 'ad')
+    client.login(username='paulina', password='ad')
     response = client.get('/footballscore/teams/')
     assert response.status_code == 200
     assert len(response.context['objects']) == 2
     for i in range(len(team)):
         assert team[i] in response.context['objects']
+
+
+@pytest.mark.django_db
+def test_season_create(client, user, season):
+    client.login(username='paulina', password='ad')
+    response = client.get('/footballscore/add_season/')
+    assert response.status_code == 200
+    assert season.season == "2019/2020"
+    assert season.league.name == 'La Liga'
+
+
+@pytest.mark.django_db
+def test_search_league(client):
+    response = client.get('/footballscore/search_league/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_detail_league(client, league):
+    response = client.get(f'/footballscore/detail_league/{league[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_search_teams(client):
+    response = client.get('/footballscore/search_teams/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_detail_team(client, team):
+    response = client.get(f'/footballscore/detail_team/{team[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_search_match(client):
+    response = client.get('/footballscore/search_match/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_league(client, user):
+    client.login(username='paulina', password='ad')
+    response = client.get('/footballscore/add_league/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_edit_league(client, user, league):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/edit_league/{league[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_league(client, user, league):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/delete_league/{league[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_team(client, user):
+    client.login(username='paulina', password='ad')
+    response = client.get('/footballscore/add_team/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_edit_team(client, user, team):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/edit_team/{team[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_team(client, user, team):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/delete_team/{team[0].pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_match(client, user):
+    client.login(username='paulina', password='ad')
+    response = client.get('/footballscore/add_match/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_edit_match(client, user, match):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/edit_match/{match.pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_match(client, user, match):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/delete_match/{match.pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_edit_season(client, user, season):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/edit_season/{season.pk}/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_delete_season(client, user, season):
+    client.login(username='paulina', password='ad')
+    response = client.get(f'/footballscore/delete_season/{season.pk}/')
+    assert response.status_code == 200
